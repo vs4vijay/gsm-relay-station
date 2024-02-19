a442ffb6e204ebc4e14972cde1ddd41737bfc503
vs4vijay <vs4vijay@gmail.com>
2024-02-19 17:18:51 +0530

    Initial Codebase for GSM Relay Station

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # gsm-relay-station
+
+## Device
+
+SIM 800L / 800H
+- LILYGO T-Call SIM800L
+- Datasheet - https://github.com/Xinyuan-LilyGO/LilyGo-T-Call-SIM800/blob/master/doc/SIM800L_IP5306.MD
+
+---
+
+## References
+
+https://github.com/vshymanskyy/TinyGSM
+https://github.com/Xinyuan-LilyGO/LilyGo-T-Call-SIM800
+
+
+---
+
+## Development Notes
+
+```bash
+
+
+
+// Restart takes quite some time
+// To skip it, call init() instead of restart()
+SerialMon.println("Initializing modem...");
+modem.restart();
+
+
+delay(10000);
+
+String imei = modem.getIMEI();
+DBG("IMEI:", imei);
+
+bool  res = modem.sendSMS(SMS_TARGET, String("Hello from ") + imei);
+DBG("SMS:", res ? "OK" : "fail");
+
+// Do nothing forevermore
+while (true) {
+    modem.maintain();
+}
+
+
+```
---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+
+#define TINY_GSM_MODEM_SIM800
+#include <TinyGSM.h>
+#include "TinyGsmCommon.h"
+
+#define LED_PIN 13
+
+#define SerialMon Serial
+#define SerialAT Serial1
+
+TinyGsm modem(SerialAT);
+
+void setup()
+{
+  SerialMon.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+
+  SerialMon.println("[+] GSM Relay Station has started...");
+
+  int min_baud_rate = 9600;
+  int max_baud_rate = 115200;
+  TinyGsmAutoBaud(SerialAT, min_baud_rate, max_baud_rate);
+  blink_led();
+  SerialMon.println("[+] GSM Relay Station: Set Auto Baud Rate");
+
+  delay(5000);
+
+  modem.init();
+  // modem.restart();
+  blink_led();
+  SerialMon.println("[+] GSM Relay Station: Init Modem");
+
+  delay(5000);
+
+  SerialMon.print("[+] GSM Relay Station: Modem Info");
+  SerialMon.println(modem.getModemInfo());
+
+  SerialMon.println("[+] GSM Relay Station: Sim Status");
+  SerialMon.println(modem.getSimStatus());
+}
+
+void loop()
+{
+  if (!modem.isNetworkConnected())
+  {
+    SerialMon.println("[+] GSM Relay Station: Network disconnected");
+    if (!modem.waitForNetwork(180000L, true))
+    {
+      SerialMon.println("[+] GSM Relay Station: Network re-connect failed");
+      delay(10000);
+      return;
+    }
+
+    if (modem.isNetworkConnected())
+    {
+      SerialMon.println("[+] GSM Relay Station: Network re-connected");
+    }
+  }
+}
+
+void blink_led() {
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+}
